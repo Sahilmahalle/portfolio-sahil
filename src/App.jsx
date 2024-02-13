13b75b4f0aa85a55aae8b42086dadda2001cbfff
Sahilmahalle <sahilvmahalle@gmail.com>
2024-02-13 08:56:09 +0530

    added tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Navbar() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="navbar flex justify-between items-center  py-2 px-4 bg-slate-950">
+      <h3 className="text-lg font-bold text-white  hover:shadow-sm">
+        Sahil Mahalle
+      </h3>
+      <div id="nav-option" className="flex spaces-x-4">
+        <h4 className="text-sm text-white px-2 py-2 ">About Me</h4>
+        <h4 className="text-sm text-white px-2 py-2">Project</h4>
+        <h4 className="text-sm text-white px-2 py-2">Skills</h4>
+        <h4 className="text-sm text-white px-2 py-2">Contact Me</h4>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default Navbar;
